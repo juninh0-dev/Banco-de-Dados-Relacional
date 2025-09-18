a97bd6d993950c8942fb163569cc609c9490e644
Altair Preto de Godoi Junior <altair.godoi@fatec.sp.gov.br>
2025-09-18 10:59:56 -0300

    Adicionamento material de aula do dia 18-09.

---
+++ 18-09/db_altair.sql
@@ -0,0 +1,11 @@
+SHOW DATABASES;
+
+-- Cria um banco de dados
+
+CREATE DATABASE IF NOT EXISTS altair;
+
+-- Deleta  um banco de dados
+
+DROP DATABASE IF EXISTS dieguinho;
+
+-- Cria um banco de dados completo!
